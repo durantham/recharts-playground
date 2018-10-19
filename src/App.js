cced521b2e2d5d42e949f96a83bdb4872900336e
EazyDev <iit2015004@iiita.ac.in>
2018-10-20 02:38:14 +0530

    Analytic Basic Components Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend,LineChart, Line, AreaChart, Area} from 'recharts';
+import {Col, Row} from 'antd';
+import 'antd/dist/antd.css';
 import './App.css';
 
 const data = [
@@ -12,14 +14,97 @@ const data = [
   {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
 ];
 
+const data2 = [
+  {x: 12},
+    {x: 24},
+      {x: 12},
+        {x: 16},
+          {x: 18},
+            {x: 20},
+              {x: 24},
+]
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <BarChart width={150} height={40} data={data}>
-          <Bar dataKey='uv' fill='#8884d8'/>
-        </BarChart>
+        <h1> VizWithThisShiz </h1>
+
+        <Row type="flex" justify="center">
+        <Col span={8}>
+        <div>
+        <AreaChart className="AppLine AppLine2" width={750} height={250} data={data}
+          margin={{ top: 10, right: 0, left: 0, bottom: 0 }}>
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="0%" stopColor="#8884d8" stopOpacity={0.8}/>
+                <stop offset="100%" stopColor="#8884d8" stopOpacity={0}/>
+              </linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="0%" stopColor="#82ca9d" stopOpacity={0.8}/>
+                <stop offset="100%" stopColor="#82ca9d" stopOpacity={0}/>
+              </linearGradient>
+            </defs>
+
+
+            <Tooltip />
+            <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+            <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+          </AreaChart>
+        </div>
+        </Col>
+        </Row>
+
+        <Row>
+        <Col span={8}>
+        <div>
+        <LineChart className="AppLine AppLine2 AppLine3" width={600} height={250} data={data2}>
+        <YAxis hide={true} type="number" domain={[10,26]}/>
+        <defs>
+          <linearGradient id="colorUp" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="0%" stopColor="#7117ea" stopOpacity={1}/>
+            <stop offset="100%" stopColor="#ea6060" stopOpacity={1}/>
+          </linearGradient>
+        </defs>
+        <Line dot={false} strokeWidth={5} type="monotone" dataKey="x" stroke="url(#colorUp)" />
+        </LineChart>
+        </div>
+        </Col>
+
+
+        <Col span={8}>
+        <div>
+        <LineChart className="AppLine AppLine2 AppLine3" width={600} height={250} data={data2}>
+        <YAxis hide={true} type="number" domain={[10,26]}/>
+        <defs>
+          <linearGradient id="colorUp" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="0%" stopColor="#7117ea" stopOpacity={1}/>
+            <stop offset="100%" stopColor="#ea6060" stopOpacity={1}/>
+          </linearGradient>
+        </defs>
+        <Line dot={false} strokeWidth={5} type="monotone" dataKey="x" stroke="url(#colorUp)" />
+        </LineChart>
+        </div>
+        </Col>
+
+
+        <Col span={7}>
+        <div>
+        <LineChart className="AppLine AppLine2 AppLine3" width={600} height={250} data={data2}>
+        <YAxis hide={true} type="number" domain={[10,26]}/>
+        <defs>
+          <linearGradient id="colorUp" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="0%" stopColor="#7117ea" stopOpacity={1}/>
+            <stop offset="100%" stopColor="#ea6060" stopOpacity={1}/>
+          </linearGradient>
+        </defs>
+        <Line dot={false} strokeWidth={5} type="monotone" dataKey="x" stroke="url(#colorUp)" />
+        </LineChart>
+        </div>
+        </Col>
+        </Row>
+
       </div>
     );
   }
